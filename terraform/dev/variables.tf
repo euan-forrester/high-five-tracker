4b2f8ce1a8797bed7cff12017853e7995ef82fe7
Euan <euan_forrester@hotmail.com>
2023-05-12 19:20:44 -0700

    Added some boilerplate

---
+++ terraform/dev/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "application_name" {
+  default = "high-five-tracker"
+}
+
+variable "notifications_email" {
+}
+
+variable "target_email" {
+}
+
+variable "project_github_location" {
+}
