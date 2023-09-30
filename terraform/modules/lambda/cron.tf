2deb41b31845337a5d4681658bafc1c3d4edd384
Euan <euan_forrester@hotmail.com>
2023-09-30 10:54:28 -0700

    Fix having cron fire our lamdba expression, plus some renaming of the function/logs

--- terraform/modules/lambda/cron.tf
+++ terraform/modules/lambda/cron.tf
@@ -1,5 +1,12 @@
 resource "aws_cloudwatch_event_rule" "cron" {
-  name        = "${var.application_name}-${var.environment}-cron-rule"
+  name        = "${var.application_name}-cron-rule-${var.environment}"
+  is_enabled = true
   description = "Fire our lambda expression"
   schedule_expression = "cron(0 16 * * ? *)"  # Run every day at 4:00 PM UTC = 9:00 AM PDT or 8:00 AM PST
+}
+
+resource "aws_cloudwatch_event_target" "cron" {
+  rule      = aws_cloudwatch_event_rule.cron.name
+  target_id = "TriggerLambda"
+  arn       = aws_lambda_function.high_fives.arn
 }
